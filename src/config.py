089b450308cf09bb0a7eafee4266c2ec24e23641
Ferrariic <thearcticfalcon@gmail.com>
2022-08-06 00:15:37 -0500

    ready for release

--- src/config.py
+++ src/config.py
@@ -13,8 +13,8 @@ TICKET_BOT = 508391840525975553
 GUILD_ID = 985750991846666284
 MATCH_CATEGORY = 993321356005486592
 ACTIVE_QUEUES_CHANNEL = 996565302202605619
-BASE = "http://127.0.0.1:8000/"
-# BASE = "http://touchgrass.online:5500/"
+# BASE = "http://127.0.0.1:8000/"
+BASE = "http://touchgrass.online:5500/"
 
 # setup logging
 file_handler = logging.FileHandler(filename="logs/error.log", mode="a")
